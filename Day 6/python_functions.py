5263da4d81e4cd56d45a1698c781fe35b7139ab5
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2022-12-10 18:12:45 +0530

    Add files via upload

---
+++ Day 6/python_functions.py
@@ -0,0 +1,14 @@
+print("Keval Panwala")  # print is a inbuilt function
+num_char = len("Keval Panwala")  # len() is a inbuilt function.
+print(num_char)
+
+
+# How to create and define our own functions      --->    def function_name():
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()  # calling of function.
+
+
---
+++ Day 6/while_loops.py
@@ -0,0 +1,9 @@
+# Syntax for while loop...
+
+# while something_is_true:
+# Do this
+# then do this
+# then do this
+
+
+
